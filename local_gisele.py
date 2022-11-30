706069ce880287fbb3a95e5674506815b79b0773
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2022-11-30 16:29:47 +0100

    Update local_gisele.py

--- local_gisele.py
+++ local_gisele.py
@@ -84,7 +84,7 @@ def create_map(latitude, longitude, sentence):
     
     # Displaying a map         
     
-    folium_static(m, width=1500, height=900)
+    folium_static(m, width=1500, height=800)
 
 @st.cache
 def uploaded_file_to_gdf(data):
