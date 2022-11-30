f669e9c3e731e719717862ae8ea0dad79e49c4e4
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2022-11-30 14:25:03 +0100

    Add files via upload

---
+++ local_gisele.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 30 14:18:38 2022
+
+@author: EDEME_D
+"""
+
+from folium.plugins import MarkerCluster
+import os
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import matplotlib.pyplot as plt
+from streamlit_folium import folium_static
+import folium
+import geopandas as gpd
+from shapely import wkt
+import xarray as xr
+import pydeck as pdk
+import ast
+import numpy as np 
+from matplotlib import cm
+import random
+import time
+from folium import Circle
+from geopandas.tools import sjoin
+
+
+
+st.set_page_config(layout="wide")
+extensionsToCheck = ('.shp', '.gpkg', '.geojson')
+name_attributes = ('name', 'Plant name', 'Plant', 'SegmentNam')
+colours = ['red', 'blue', 'green', 'purple', 'orange', 'darkred', 'lightred', 'beige', 'darkblue', 'darkgreen', 'cadetblue', 'darkpurple', 'white', 'pink', 'lightblue', 'lightgreen', 'gray', 'black', 'lightgray']
+
+st.write("Ciao")
