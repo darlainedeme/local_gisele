985937ae5c447a9aeac0a29df8c2813f19e09676
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2022-12-16 18:22:46 +0100

    lights

--- local_gisele.py
+++ local_gisele.py
@@ -357,10 +357,10 @@ elif which_mode == 'Upload file':
 
         data.values[data.values < 0] = np.nan
 
-        
+
 
         # lights = "light.tif"
-        # lights = None
+        lights = None
 
         create_map(data_gdf.centroid.y, data_gdf.centroid.x, False, data_gdf, gdf_edges, buildings_save, pois, lights)
 
