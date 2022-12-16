7a4ded06b918e1c7c8fd59fe1d7db102da70dee8
Darlain Edeme <45342315+darlainedeme@users.noreply.github.com>
2022-12-16 02:23:33 +0100

    Update local_gisele.py

--- local_gisele.py
+++ local_gisele.py
@@ -336,7 +336,7 @@ elif which_mode == 'Upload file':
         # Grab the first item from the search results and sign the assets
         first_item = next(search.items())
         
-        with requests.get(pc.sign_item(first_item).assets.get('lightscore').href) as response:
+        with requests.get(pc.sign_item(first_item, copy=True).assets.get('lightscore').href) as response:
             response = requests.get(pc.sign_item(first_item).assets.get('lightscore').href)
             file = open("light.tif", "wb")
             file.write(response.content)
